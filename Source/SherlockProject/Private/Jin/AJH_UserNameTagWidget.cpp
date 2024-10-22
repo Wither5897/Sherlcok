abe081966a52a3502b275cf3c47a6996272a826f
jinheyong <soogo880@gmail.com>
2024-10-22 10:05:31 +0900

    Commit 시작 UI, 크리에이팅툴 UI 디자인 작업
    시작 UI, 크리에이팅툴 UI 디자인 작업

---
+++ Source/SherlockProject/Private/Jin/AJH_UserNameTagWidget.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Jin/AJH_UserNameTagWidget.h"
+#include "Components/TextBlock.h"
+
+
+void UAJH_UserNameTagWidget::SetUserName(FString value)
+{
+	Txt_UserName->SetText(FText::FromString(value));
+}
---
+++ Source/SherlockProject/Public/Jin/AJH_UserNameTagWidget.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AJH_UserNameTagWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHERLOCKPROJECT_API UAJH_UserNameTagWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly,meta=(BindWidget))
+	class UTextBlock* Txt_UserName;
+
+	UFUNCTION()
+	void SetUserName(FString value);
+
+};
