d4436750370f26c3d71d6b016dec95c4a511b0c9
jinheyong <soogo880@gmail.com>
2024-10-28 16:46:51 +0900

    멀티 브리핑 제작 중
    멀티 브리핑 제작 중

--- Source/SherlockProject/Private/Jin/AJH_SummaryWidget.cpp
+++ Source/SherlockProject/Private/Jin/AJH_SummaryWidget.cpp
@@ -1,5 +1,50 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "Jin/AJH_SummaryWidget.h"
+#include "Components/Button.h"
+#include "SK/InventoryWidget.h"
+#include "Kismet/GameplayStatics.h"
 
+void UAJH_SummaryWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+	if ( Btn_Click )
+	{
+		Btn_Click->OnClicked.AddDynamic(this, &UAJH_SummaryWidget::OnMyBtn_Click);
+	}
+
+	// InventoryUI가 null인 경우 생성
+	if ( !InventoryUI )
+	{
+		APlayerController* PC = UGameplayStatics::GetPlayerController(this, 0);
+		if ( PC )
+		{
+			InventoryUI = CreateWidget<UInventoryWidget>(PC, UInventoryWidget::StaticClass());
+			if ( !InventoryUI )
+			{
+				UE_LOG(LogTemp, Error, TEXT("InventoryUI 위젯을 생성할 수 없습니다."));
+			}
+		}
+		else
+		{
+			UE_LOG(LogTemp, Error, TEXT("PlayerController를 가져올 수 없습니다."));
+		}
+	}
+}
+
+void UAJH_SummaryWidget::OnMyBtn_Click()
+{
+	if ( InventoryUI && InventoryUI->SuspectImage )
+	{
+		Img_Inven = InventoryUI->SuspectImage;
+		UE_LOG(LogTemp, Warning, TEXT("살려주세여"));
+		UE_LOG(LogTemp, Warning, TEXT("Yes"), Img_Inven);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("InventoryUI 또는 SuspectImage가 null 상태입니다."));
+	}
+	
+}
--- Source/SherlockProject/Public/Jin/AJH_SummaryWidget.h
+++ Source/SherlockProject/Public/Jin/AJH_SummaryWidget.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -14,4 +14,18 @@ class SHERLOCKPROJECT_API UAJH_SummaryWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void NativeConstruct() override;
+
+	UPROPERTY(meta=(BindWidget))
+	class UButton* Btn_Click;
+	UPROPERTY(meta=(BindWidget))
+	class UImage* Img_Inven;
+
+	UPROPERTY(EditDefaultsOnly)
+	class UInventoryWidget* InventoryUI;
+
+	UFUNCTION()
+	void OnMyBtn_Click();
+
 };
