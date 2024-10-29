b2cebd11d8f1a205e7cd7661eab105fa098bce12
ghkgus407 <143527072+ghkgus407@users.noreply.github.com>
2024-10-29 18:06:48 +0900

    Update SherlockProject.bat

--- SherlockProject.bat
+++ SherlockProject.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Epic Games\UE_5.4\Engine\Binaries\Win64\UnrealEditor.exe" "C:\Users\Admin\Documents\Unreal Projects\Sherlcok\SherlockProject.uproject" "LobbyMap" -game -log -ResX=800 -ResY=600 -WinX=100 -WinY=100
+"C:\Program Files\Epic Games\UE_5.4\Engine\Binaries\Win64\UnrealEditor.exe" "C:\project\Sherlcok\SherlockProject.uproject" "LobbyMap" -game -log -ResX=800 -ResY=600 -WinX=100 -WinY=100
