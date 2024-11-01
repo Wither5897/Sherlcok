ecda6e984f531a750d45e55b260ae24c03a612c5
jinheyong <soogo880@gmail.com>
2024-11-01 18:16:30 +0900

    Travel 클라이언트 UI 제작 중
    Travel 클라이언트 UI 제작 중

--- Source/SherlockProject/Public/Jin/AJH_TravelClientWidget.h
+++ Source/SherlockProject/Public/Jin/AJH_TravelClientWidget.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -13,5 +13,23 @@ UCLASS()
 class SHERLOCKPROJECT_API UAJH_TravelClientWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+protected:
+	virtual void NativeConstruct() override;
 	
+public:
+
+	UPROPERTY(meta=(BindWidget))
+	class UButton* Btn_FirstCrimeReady;
+	UPROPERTY(meta=(BindWidget))
+	class UButton* Btn_Cancel;
+
+	UFUNCTION()
+	void OnMyBtn_FirstCrimeReady();
+	UFUNCTION()
+	void OnMyBtn_Cancel();
+
+	UPROPERTY()
+	class ASherlockPlayerController* SherlockPC;
+
 };
