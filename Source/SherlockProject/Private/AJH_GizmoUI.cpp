4a77ab30d95c63c2f7b498b10477f8a55c890cf0
jinheyong <soogo880@gmail.com>
2024-11-11 17:03:29 +0900

    액터 선택시 Location 좌표 값 Widget에 반영
    액터 선택시 Location 좌표 값 Widget에 반영
    Location 값 변경 시 Widget에 반영

---
+++ Source/SherlockProject/Private/AJH_GizmoUI.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AJH_GizmoUI.h"
+#include "Components/WidgetSwitcher.h"
+#include "Jin/AJH_EditorCharacter.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/EditableText.h"
+
+void UAJH_GizmoUI::NativeConstruct()
+{
+	player = Cast<AAJH_EditorCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+	Switcher_GizmoUI->SetActiveWidgetIndex(0);
+	OnMyEdit_Location();
+	GetEdit_Location();
+
+}
+
+void UAJH_GizmoUI::OnMyEdit_Location()
+{
+	if ( player )
+	{
+		Location_X = FString::SanitizeFloat(player->newLocation.X);
+		// UEditableText에 값 설정
+		Edit_Location_X->SetText(FText::FromString(Location_X));
+		Location_Y = FString::SanitizeFloat(player->newLocation.Y);
+		Edit_Location_Y->SetText(FText::FromString(Location_Y));
+		Location_Z = FString::SanitizeFloat(player->newLocation.Z);
+		Edit_Location_Z->SetText(FText::FromString(Location_Z));
+	}
+}
+
+void UAJH_GizmoUI::GetEdit_Location()
+{
+	Location_X = FString::SanitizeFloat(actorLocation.X);
+	Edit_Location_X->SetText(FText::FromString(Location_X));
+	Location_Y = FString::SanitizeFloat(actorLocation.Y);
+	Edit_Location_Y->SetText(FText::FromString(Location_Y)); 
+	Location_Z = FString::SanitizeFloat(actorLocation.Z);
+	Edit_Location_Z->SetText(FText::FromString(Location_Z));
+}
+
---
+++ Source/SherlockProject/Public/AJH_GizmoUI.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AJH_GizmoUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHERLOCKPROJECT_API UAJH_GizmoUI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+
+public:
+	virtual void NativeConstruct() override;
+
+	UPROPERTY(EditDefaultsOnly, meta=(bindWidget))
+	class UWidgetSwitcher* Switcher_GizmoUI;
+	UPROPERTY(EditDefaultsOnly, meta=(bindWidget))
+	class UEditableText* Edit_Location_X;
+	UPROPERTY(EditDefaultsOnly, meta=(bindWidget))
+	class UEditableText* Edit_Location_Y;
+	UPROPERTY(EditDefaultsOnly, meta=(bindWidget))
+	class UEditableText* Edit_Location_Z;
+
+	void OnMyEdit_Location();
+	void GetEdit_Location();
+
+	FString Location_X;
+	FString Location_Y;
+	FString Location_Z;
+
+	FVector actorLocation;
+
+	UPROPERTY()
+	class AAJH_EditorCharacter* player;
+
+};
