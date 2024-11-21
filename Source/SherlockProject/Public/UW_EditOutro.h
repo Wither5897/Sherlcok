24686a6c42756121c2718e381c7aaf145e6b106f
ghkgus407 <143527072+ghkgus407@users.noreply.github.com>
2024-11-21 13:31:47 +0900

    Edit Intro, Outro

---
+++ Source/SherlockProject/Public/UW_EditOutro.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UW_EditOutro.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHERLOCKPROJECT_API UUW_EditOutro : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadOnly, meta = ( BindWidget ))
+	class UMultiLineEditableText* EndingMessage;
+
+	UPROPERTY(BlueprintReadOnly, meta = ( BindWidget ))
+	class UButton* StorySave;
+	
+};
